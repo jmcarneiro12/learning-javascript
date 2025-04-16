3e4497811ccb2397ffda1cacca1ee6d980581b4e
jmcarneiro12 <jmcarneirodantas1412@gmail.com>
2025-04-16 10:11:58 -0400

    added a new foldere speedAnalysis

--- testing/speedAnalysis/speed_analysis.js
+++ testing/speedAnalysis/speed_analysis.js
@@ -30,9 +30,14 @@ function startTest(){
     if (timeElapsed !== 0 && !isNaN(typedWords)) {
         wpm = Math.round((typedWords / timeElapsed) * 60);
     }
+    var outputLength = 0; // Default
+    if (!isNaN(typedWords)){
+        outputLength = userTypedText.length;
+    }
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + outputLength + " characters</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
